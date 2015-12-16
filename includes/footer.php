01a213bc5874ec11d58b1591b532e5d63b666571
ErinEMcC <erinm35@gmail.com>
2015-12-16 01:37:49 -0500

    Includes
    include php files

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 </div>
 <footer>
-Copyright <?php echo date("Y") ?>
+Copyright <?php echo date("Y") ?><br>
 Data provided by <a href = "http://www.rochester.edu/College/translation/threepercent/">Three Percent</a> at the University of Rocherster.<br>
 "Not knowing what the rest of the world is thinking and writing is both dangerous and boring." <a href = "http://thenewpress.com/books/literature-from-axis-of-evil">(Alane Mason) </a>
 </footer>
--- includes/header1215.php
+++ includes/header1215.php
@@ -2,9 +2,9 @@
 <html>
 <head>
 		<meta charset="UTF-8" />
-		<title>Literature from around the World</title>
+		<title>Translations</title>
 
-<link href="includes/style2.css" rel="stylesheet" type="text/css"/>
+<link href="style2.css" rel="stylesheet" type="text/css"/>
 <link href="cssmenu/styles.css" rel="stylesheet" type="text/css"/>
 <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
 <!--<script src="script.js"></script>-->
@@ -23,3 +23,4 @@
 </header>
 
 <div class="container">
+<h1>Literature from around the World</h1>
