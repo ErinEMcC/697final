1e127d94cac17ec710b34c3019a4dae9e5c7d7f6
ErinEMcC <erinm35@gmail.com>
2015-12-16 13:16:47 -0500

    recent translations published
    where pub_year = YEAR(CURRENT_TIMESTAMP)

--- latest.php
+++ latest.php
@@ -2,10 +2,11 @@
 session_start();
 include_once 'includes/header1215.php';
 ?>
-<h2>Translated Books from 2015</h2>
-<script type='text/javascript' src='https://public.tableau.com/javascripts/api/viz_v1.js'></script><div class='tableauPlaceholder' style='width: 982px; height: 742px;'><noscript><a href='#'><img alt='Sheet 2 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Tr&#47;Translations_2015&#47;Sheet2&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz' width='982' height='742' style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='site_root' value='' /><param name='name' value='Translations_2015&#47;Sheet2' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Tr&#47;Translations_2015&#47;Sheet2&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='showVizHome' value='no' /><param name='showTabs' value='y' /><param name='bootstrapWhenNotified' value='true' /></object></div>
-<?php
 
+<h2>Translated Books from this Year</h2>
+<script type='text/javascript' src='https://public.tableau.com/javascripts/api/viz_v1.js'></script><div class='tableauPlaceholder' style='width: 982px; height: 742px;'><noscript><a href='#'><img alt='Sheet 2 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Tr&#47;Translations_2015&#47;Sheet2&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz' width='982' height='742' style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='site_root' value='' /><param name='name' value='Translations_2015&#47;Sheet2' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Tr&#47;Translations_2015&#47;Sheet2&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='showVizHome' value='no' /><param name='showTabs' value='y' /><param name='bootstrapWhenNotified' value='true' /></object></div>
+
+<?php
 require_once 'includes/login.php';
 
 $conn = new mysqli($hn, $un, $pw, $db);
@@ -13,11 +14,12 @@ if ($conn->connect_error) die($conn->connect_error);
 
 $query = "SELECT titles.title, 
 	titles.language, 
-	countries.countryDisplay, 
+	titles.pub_year,
+	countries.countryDisplay
 	FROM titles
 	INNER JOIN countries
 	ON titles.countryCode = countries.countryCode
-	WHERE titles.pub_year = 2015";
+	WHERE titles.pub_year = YEAR(CURRENT_TIMESTAMP)";
 	
 	
 $result = $conn->query($query);
@@ -34,7 +36,7 @@ if ($result->num_rows>0){
 
 }		
 	else {
-	echo "0 Titles Availble";
+	echo "0 Titles Available";
 }
 
 $conn->close();
